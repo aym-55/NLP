ebd2728b45640af57ddc4e3da28fabeba210c08f
aym-55 <aymeric.legrand7777@hotmail.com>
2021-12-16 21:04:38 +0100

    Ajout fichier stemming.py à la branche nlp-tf

--- stemming.py
+++ stemming.py
@@ -1,4 +1,17 @@
 class Stemming():
-    def __init__(self):
-        pass
+    def __init__(self, mot):
+        
+        self.mot = mot
+        self.end_words = ["eur","euse"]
 
+    def test(self):
+
+        for i in self.end_words:
+            if i in self.mot:
+                print(self.mot)
+            else:
+                print("ça marche pas")
+                
+
+test1 = Stemming("chanteur")
+print(test1.test())
